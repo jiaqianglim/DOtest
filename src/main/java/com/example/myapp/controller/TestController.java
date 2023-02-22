25506ff79aa5b182884046e32ea856ad53562927
jiaqianglim <jqlimjiaqiang@gmail.com>
2023-02-22 13:25:16 +0800

    first commit

--- src/main/java/com/example/myapp/controller/TestController.java
+++ src/main/java/com/example/myapp/controller/TestController.java
@@ -1,12 +1,13 @@
 package com.example.myapp.controller;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
 
 @RequestMapping("/")
 @RestController
@@ -19,14 +20,14 @@ public class TestController {
 
     @GetMapping(path = "/string")
     public ResponseEntity<String> getString() {
-        return ResponseEntity.ok().body("Spring Works");
+        JsonObject sample = Json.createObjectBuilder().add("sample string 1", "ok").add("sample string 2", "ok")
+                .build();
+        return ResponseEntity.ok().body(sample.toString());
     }
 
     @GetMapping(path = "/array")
     public ResponseEntity<String> getList() {
-        List sampleList = new LinkedList<String>();
-        sampleList.add("string 1");
-        sampleList.add("string 2");
-        return ResponseEntity.ok().body(sampleList.toString());
+        JsonArray sample = Json.createArrayBuilder().add("sample string 1").add("sample string 2").build();
+        return ResponseEntity.ok().body(sample.toString());
     }
 }
